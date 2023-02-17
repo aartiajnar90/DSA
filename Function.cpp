58d68dff46db735eb29ad774e181c0ee3fc2a183
aartiajnar90 <125665931+aartiajnar90@users.noreply.github.com>
2023-02-17 18:35:06 +0530

    Create Function.cpp

---
+++ Function.cpp
@@ -0,0 +1,18 @@
+#include namespace std;
+using namespace std;
+int maximum(int x, int y)
+{
+	if(x>y)
+	return x;
+	else
+	return y;
+}
+int main()
+
+{
+	int a, b;
+	cout<<"Enter 2 numbers to find the greatest number:"<<endl;
+	cin>>a>>b;
+	cout<<"another greatest value out of 5, 6:"<<endl;
+	return 0;
+}
